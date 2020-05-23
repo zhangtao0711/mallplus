3dffcc2b022bf2ad6c0f8d8f43456c9f55a68b42
zhangyuhan <zzzzyh1999@163.com>
2020-05-23 09:59:06 +0800

    ui框架配置

--- mall-web-admin-mp-app/src/main.js
+++ mall-web-admin-mp-app/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 
 import 'normalize.css/normalize.css'// A modern alternative to CSS resets
 
+import {Card } from 'vant';
+Vue.use(Card);
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
